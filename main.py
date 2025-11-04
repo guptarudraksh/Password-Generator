869a3c861ffb6a44029494f1037488146f06490b
guptarudraksh <guptarudraksh6@gmail.com>
2025-11-05 04:23:20 +0530

    Add password length validation to password generator

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ if __name__ == "__main__":
     s2 = string.ascii_uppercase
     s3 = string.digits
     s4 = string.punctuation
+
     plen = int(input("Enter the password length:\n"))
-    s = []
-    s.extend(list(s1))
-    s.extend(list(s2))
-    s.extend(list(s3))
-    s.extend(list(s4))
-    random.shuffle(s)
-    # print(s)
-    print("Your password is:")
-    print("".join(s[0:plen]))
+
+    if plen <= 0:
+        print("Password length must be greater than 0!")
+    else:
+        s = []
+        s.extend(list(s1))
+        s.extend(list(s2))
+        s.extend(list(s3))
+        s.extend(list(s4))
+        random.shuffle(s)
+        print("Your password is:")
+        print("".join(s[0:plen]))
